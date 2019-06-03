e052e043fa2b3bdd6417382efe118b9c4e821c97
axionos <axionoso@gmail.com>
2019-06-03 16:21:10 -0400

    test

---
+++ src/data.js
@@ -0,0 +1,61 @@
+Const data = {
+
+  "gameInfo" :  {
+      "name" : "lutrivia",
+      "score" : 1
+  },
+  "questions" : [
+    {
+      text: "An otter's den is called a sofa.",
+      answer: false,
+      selected: true
+    },
+    {
+      text: "The word otter is related to the English word water.",
+      answer: true,
+      selected: true
+    },
+    {
+      text: "Otter pups stay with their families for up to one year.",
+      answer: true,
+      selected: true
+    },
+    {
+      text: "Otters stay warm through a combination of blubber and fur.",
+      answer: false,
+      selected: true
+    },
+    {
+      text: "Otters commonly engage in playful behaviors.",
+      answer: true,
+      selected: true
+    },
+    {
+      text: "There are only three existing otter species: sea otter, ",
+      answer: false,
+      selected: true
+    },
+    {
+      text: "Fish is a staple of the otter diet.",
+      answer: true,
+      selected: true
+    },
+    {
+      text: "The otter gestation period is between 60 to 86 days.",
+      answer: true,
+      selected: true
+    },
+    {
+      text: "Otters live all over the world.",
+      answer: true,
+      selected: true
+    },
+    {
+      text: "Otters show up in myths from different cultures.",
+      answer: true,
+      selected: true
+    }
+  ]
+
+
+}
