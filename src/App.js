3dbe504b391365dfbe1788af977a35ba428939c8
axionos <axionoso@gmail.com>
2019-06-05 11:39:07 -0400

    minor change for  App state from game to gameOn

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import QuestionsContainer from './QuestionsContainer.js';
 class App extends React.Component{
   // 1. set the "shared state"
   state={
-    game: false
+    gameOn: false
   }
 
   // 4. switch the game status to "true"
   startGame = (event) => {
     this.setState({
-      game: true
+      gameOn: !this.state.gameOn
     })
   }
 
@@ -24,11 +24,11 @@ class App extends React.Component{
         name={data.gameInfo.name}
         score={data.gameInfo.score}
         startGame={this.startGame} // 5. pass the function as a prop to the child
-        inProgress={this.state.game} // 2. share the state with the children
+        inProgress={this.state.gameOn} // 2. share the state with the children
       />
       <QuestionsContainer
         questionsList={data.questions}
-        inProgress={this.state.game} // 2. share the state with the children
+        inProgress={this.state.gameOn} // 2. share the state with the children
       />
       </div>
     );
--- src/Question.js
+++ src/Question.js
@@ -2,28 +2,28 @@ import React from 'react';
 
 
 class Question extends React.Component {
-  
-  answerTrue = (event) => {
-    this.setState({
-      isSelected: true
-    })
-  }
-  answerFalse = (event) => {
-    this.setState({
-      isSelected: false
-    })
-  }
 
-  handleClickTrue = (event) => {
-    if (this.answerTrue === this.props.answer) {
-      return <button style={this.props.answer ? {backgroundColor: "#39ff14"} : {backgroundColor:"red"}} />
-    }
-  }
-  handleClickFalse = (event) => {
-    if (this.state.isSelected === this.props.answer) {
-
-    }
-  }
+  // answerTrue = (event) => {
+  //   this.setState({
+  //     isSelected: true
+  //   })
+  // }
+  // answerFalse = (event) => {
+  //   this.setState({
+  //     isSelected: false
+  //   })
+  // }
+  //
+  // handleClickTrue = (event) => {
+  //   if (this.answerTrue === this.props.answer) {
+  //     return <button style={this.props.answer ? {backgroundColor: "#39ff14"} : {backgroundColor:"red"}} />
+  //   }
+  // }
+  // handleClickFalse = (event) => {
+  //   if (this.state.isSelected === this.props.answer) {
+  //
+  //   }
+  // }
   render() {
     // style={this.props.answer ? {backgroundColor: "#39ff14"} : {backgroundColor:"red"}}
 
